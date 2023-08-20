80620d317aea6cc7ecde610fd855f4f8fad9ef86
Kingsley Owolabi <75090590+Kingsley320@users.noreply.github.com>
2023-08-20 20:41:32 +0100

    Create Script.cpp
    A simple C++ Script that creates a random array of  numbers with length gotten from the user and sorts them in descending order.

---
+++ Script.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#define MAX 100
+using namespace std;
+
+int main(){
+    int n;
+    int arr[MAX];
+    cout<<"Enter a number N: "<< endl;
+    cin>>n;
+    
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = rand();
+    }
+    
+    for(int i = 0; i<n; i++){
+        cout<< arr[i]<<" ";
+        
+    }
+    cout<<endl;
+    
+    // sorting array
+    for (int i = 0; i<n; i++){
+    // Assending or descending depends on greater than / less than
+        for(int j=i+1; j<n; j++){
+            if(arr[j] > arr[i]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    // display sorted array
+    
+      for(int i = 0; i<n; i++){
+        cout<< arr[i]<<" ";
+        
+    }
+    cout<<endl;
+    return 0;
+    
+}
