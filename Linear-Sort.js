97558cef36a7cbd9acc3ca4bedfa78d4221dda8f
Kingsley Owolabi <75090590+Kingsley320@users.noreply.github.com>
2023-08-21 22:24:07 +0100

    Create Linear-Sort.js
    Sorts an array in ascending order in linear time

---
+++ Linear-Sort.js
@@ -0,0 +1,24 @@
+//Linear sort algorithm
+// let arr = [3,5,6,1,2,3,5,6,7,8,1];
+
+// function min(arr){
+//   let k = arr[0]
+//   for(let i = 0; i<arr.length; i++){
+//     if(arr[i] < k){
+//       k = arr[i]
+//     }
+//   }
+//   return k;
+// }
+
+// function linear(arr){
+//   let result = []
+//   while(arr.length > 0){
+//     let d = min(arr)
+//     let index = arr.indexOf(d)
+//     result.push(d)
+//     arr.splice(index,1)
+//   }
+//   return result;
+// }
+// console.log(linear(arr))
