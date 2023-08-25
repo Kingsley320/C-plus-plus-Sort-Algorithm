5dbffa42bcb2412237bd55a7d7dcd83c1e13a9cb
Kingsley Owolabi <75090590+Kingsley320@users.noreply.github.com>
2023-08-25 01:34:55 +0100

    Update Subsets.js
    Not passing most test cases. Need to push universal sets for all subsets

--- Subsets.js
+++ Subsets.js
@@ -1,1 +1,28 @@
+var subsets = function(nums) {
+    
+  let arr=[];
+  arr.push([])
+  for (let i = 0; i<nums.length; i++){
+    let k=0;
+    let j = i;
+    let subarr = [];
+    let fullset = []
+    arr.push([nums[i]])
+    fullset.push(nums[i])
+    while(j != 0){
+      subarr.push(nums[k],nums[i])
+      arr.push(subarr)
+      subarr = [];
+      k++
+      j--
+    }
+    let l = [...fullset, nums[i+1]];
+//     console.log(fullset, l)
+//     if(arr.includes(l)){
+//     arr.push(l)
+//     }
+  }
+  return arr
+};
 
+console.log(subsets([1,2,3]))
